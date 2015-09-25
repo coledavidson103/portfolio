706e7df178c474fcbbae0bc5bf6f6b7a7482bb33
Cole Davidson <coledavidson@Coles-MacBook-Pro.local>
2015-09-25 18:12:00 -0400

    Installed Isotope plugin to assist with styling filter (filters using classes).

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // Determine the width of the window
 var w = $(window).width();
 
+// Determine the height of the window
+var h = $(window).height();
+
 // Fade in the mobile menu if the menu button is clicked
 $('.mobile-nav').on('click', function(){
 	$('nav').toggleClass('mobile-nav-show');
@@ -21,14 +24,52 @@ $('#scroller-link').on('click', function() {
 });
 
 // Paralax scroll the heading
+$(window).on('scroll', function() {
+	var px = $(window).scrollTop();
+	$('.page-title-container').css('top', -px);
 
-// Portfolio sorting
+	var o = px / h ;
+	
+	if(px > 0) {
+		$('.page-title-container').css('opacity', 1 - o);
+	};
+});
+
+// Isotope
+$('.grid').isotope({
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+
+// Show client sorting options
 $('#client-sort').on('click', function() {
 	$('#client-list').toggleClass('sort-select-show');
 });
 
-$('#vancouver-sort').on('click', function() {
-	$('.work').css('display', 'none');
-	$('.vancouver').css('display', 'block');
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
 });
+// filter items on button click
+$('.filter-button-group').on( 'click', 'li', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+// Title changes
+$('#ylc').on('click', function () {
+	$('.workspace h2').text('Client: Young Liberals of Canada');
+});
+
+$('#vancouver').on('click', function () {
+	$('.workspace h2').text('Client: City of Vancouver');
+});
+
+
+// Manual filtering
+// $('#vancouver-sort').on('click', function() {
+// 	$('.work').css('display', 'none');
+// 	$('.vancouver').css('display', 'block');
+// });
 
