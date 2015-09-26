857f28efb382ea3b4f7f9942ae6e4781bba4fc35
Cole Davidson <coledavidson@Coles-MacBook-Pro.local>
2015-09-26 17:51:44 -0400

    Adding styling to the workspace layout, added a lightbox plugin (still not working properly)

--- js/script.js
+++ js/script.js
@@ -1,75 +1,74 @@
-// Determine the width of the window
-var w = $(window).width();
+$(document).ready(function () {	
 
-// Determine the height of the window
-var h = $(window).height();
+	// Load Fancybox
+	$(".fancybox").fancybox();
 
-// Fade in the mobile menu if the menu button is clicked
-$('.mobile-nav').on('click', function(){
-	$('nav').toggleClass('mobile-nav-show');
-});
+	// Determine the width of the window
+	var w = $(window).width();
 
-// If the service button in the menu is clicked,
-// the screen width is less than 769 (mobile), toggle .submenu-show class
-// to show the submenu.
-if(w < 769) {
-	$('.service-button').on('click', function () {
-				$('.services-menu').toggleClass('submenu-show');
+	// Determine the height of the window
+	var h = $(window).height();
+
+	// Fade in the mobile menu if the menu button is clicked
+	$('.mobile-nav').on('click', function(){
+		$('nav').toggleClass('mobile-nav-show');
 	});
-};
 
-// Animate the scroll when clicking on the scroller arrow
-$('#scroller-link').on('click', function() {
-	$('#description').animatescroll();
-});
-
-// Paralax scroll the heading
-$(window).on('scroll', function() {
-	var px = $(window).scrollTop();
-	$('.page-title-container').css('top', -px);
-
-	var o = px / h ;
-	
-	if(px > 0) {
-		$('.page-title-container').css('opacity', 1 - o);
+	// If the service button in the menu is clicked,
+	// the screen width is less than 769 (mobile), toggle .submenu-show class
+	// to show the submenu.
+	if(w < 769) {
+		$('.service-button').on('click', function () {
+					$('.services-menu').toggleClass('submenu-show');
+		});
 	};
+
+	// Animate the scroll when clicking on the scroller arrow
+	$('#scroller-link').on('click', function() {
+		$('#description').animatescroll();
+	});
+
+	// Paralax scroll the heading
+	$(window).on('scroll', function() {
+		var px = $(window).scrollTop();
+		$('.page-title-container').css('top', -px);
+
+		var o = px / h ;
+		
+		if(px > 0) {
+			$('.page-title-container').css('opacity', 1 - o);
+		};
+	});
+
+	// Isotope
+	$('.grid').isotope({
+	  // options
+	  itemSelector: '.grid-item',
+	  layoutMode: 'fitRows'
+	});
+
+	// Show client sorting options
+	$('#client-sort').on('click', function() {
+		$('#client-list').toggleClass('sort-select-show');
+	});
+
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  // options
+	});
+	// filter items on button click
+	$('.filter-button-group').on( 'click', 'li', function() {
+	  var filterValue = $(this).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
+	});
+
+	// Title changes
+	$('#ylc').on('click', function () {
+		$('.workspace h2').text('Client: Young Liberals of Canada');
+	});
+
+	$('#vancouver').on('click', function () {
+		$('.workspace h2').text('Client: City of Vancouver');
+	});
+
 });
-
-// Isotope
-$('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
-});
-
-// Show client sorting options
-$('#client-sort').on('click', function() {
-	$('#client-list').toggleClass('sort-select-show');
-});
-
-// init Isotope
-var $grid = $('.grid').isotope({
-  // options
-});
-// filter items on button click
-$('.filter-button-group').on( 'click', 'li', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});
-
-// Title changes
-$('#ylc').on('click', function () {
-	$('.workspace h2').text('Client: Young Liberals of Canada');
-});
-
-$('#vancouver').on('click', function () {
-	$('.workspace h2').text('Client: City of Vancouver');
-});
-
-
-// Manual filtering
-// $('#vancouver-sort').on('click', function() {
-// 	$('.work').css('display', 'none');
-// 	$('.vancouver').css('display', 'block');
-// });
-
